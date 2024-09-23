9b00a91ca62ff3a1e6dcab1adaf147bdd62ea861
krstie <kristietran010308@gmail.com>
2024-09-23 11:21:26 -0700

    hi

--- Main.java
+++ Main.java
@@ -2,12 +2,27 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // test your functions here to make sure they work
+    double distNum = distFormula(1, 1, 2, 2);
+    System.out.println(distNum);
   }
+  public static double distNum(double x1. double y1, double x2, double y2)
+  {
+    double distX = (x1-x2)+(x1-x2);
+    double distY = (y1-y2)+(y1-y2);
+    double dist = Math.sqrt(distX+distY);
+    return dist;
+  }
+  
 
-  // Implement distance formula method here
 
-  // implement one of your quadratic formula methods here
-
-  // implement the other quadratic formula here
-}
+  public static void main(String[] args)
+  {
+    double quadNum quadFormula(4, 6, 1);
+    System.out.println(quadNum);
+  }
+  public static double quadNum(double a, double b, double c);
+  {
+    double quadA = (2*a);
+    double quadB = (b^2-(4*a*c));
+    double quadC
+  }
